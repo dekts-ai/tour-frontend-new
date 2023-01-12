66543e0644c41cc23164ae8811ce60bbb2803750
Devat Karetha <devat@glorywebsdev.com>
2023-01-12 16:11:57 +0530

    Fixed the grey screen issue in the platfrom

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@ export default {
     var date = new Date();
     if (this.$store.state.date) {
       date = new Date(this.$store.state.date);
-    } else if (localStorage.getItem('date')) {
+    } else if (typeof localStorage.getItem("date") !== 'undefined') {
       date = new Date(localStorage.getItem('date'));
     }
 
