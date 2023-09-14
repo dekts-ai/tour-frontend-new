e1a7c481e4d2503ebf7a77bde3fba690180fff55
Devat Karetha <devat@glorywebs.com>
2023-09-14 17:43:14 +0530

    Fixed the iframe design issue in the frontend package list page

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -11,49 +11,53 @@
     <section class="inner-content-section">
         <div :class="[(iframeStatus == false) ? 'container' : 'no-container', '']">
             <div class="background-color-sec">
-                <div class="row payment-row">
+                <div class="row">
                     <div class="col-12">
-                        <div class="row booking-row">
-                            <div class="col-lg-6 col-md-12">
-                                <div class="booking">
-                                    <h2>Book Online</h2>
-                                    <div class="confirmation">
-                                        <img src="../assets/images/confirmation.png">
-                                        <p>Get Instant Confirmation</p>
+                        <div class="row payment-row">
+                            <div class="col-12">
+                                <div class="row booking-row">
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="booking">
+                                            <h2>Book Online</h2>
+                                            <div class="confirmation">
+                                                <img src="../assets/images/confirmation.png">
+                                                <p>Get Instant Confirmation</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12 text-center text-lg-end text-md-center text-sm-center">
+                                        <div class="info">
+                                            <button class="tooltipbtn btn-info" data-toggle="tooltip" data-placement="top"
+                                                title="">Secured</button>
+                                            <button class="tooltipbtn btn-danger" data-toggle="tooltip" data-placement="top"
+                                                title="">Health &
+                                                Safety</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-6 col-md-12 text-center text-lg-end text-md-center text-sm-center">
-                                <div class="info">
-                                    <button class="tooltipbtn btn-info" data-toggle="tooltip" data-placement="top"
-                                        title="">Secured</button>
-                                    <button class="tooltipbtn btn-danger" data-toggle="tooltip" data-placement="top"
-                                        title="">Health &
-                                        Safety</button>
-                                </div>
-                            </div>
-                        </div>
-                        <form @submit.prevent="submit">
-                            <div class="row starttime-row">
-                                <div v-for="tourPackage in TourPkgDetails" :key="TourPkgDetails.id" class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal deal-block">
-                                    <div class="item-slide">
-                                        <div class="box-img">
-                                            <img :src="tourPackage.FrontendPackageImage"
-                                                :alt="tourPackage.TourPkgName" />
-                                            <div class="text-wrap">
-                                                <h4>{{ tourPackage.TourPkgName }}</h4>
-                                                <div class="desc">
-                                                    <span>{{ tourPackage.TourPkgDuration }}</span>
-                                                </div>
-                                                <div class="book-now-c">
-                                                    <button @click="bookNow(tourOperatorId, tourPackage.TourPackageId)">Book Now</button>
+                                <form @submit.prevent="submit">
+                                    <div class="row starttime-row">
+                                        <div v-for="tourPackage in TourPkgDetails" :key="TourPkgDetails.id" class="col-lg-4 col-md-4 col-sm-6 col-xs-12 deal deal-block">
+                                            <div class="item-slide">
+                                                <div class="box-img">
+                                                    <img :src="tourPackage.FrontendPackageImage"
+                                                        :alt="tourPackage.TourPkgName" />
+                                                    <div class="text-wrap">
+                                                        <h4>{{ tourPackage.TourPkgName }}</h4>
+                                                        <div class="desc">
+                                                            <span>{{ tourPackage.TourPkgDuration }}</span>
+                                                        </div>
+                                                        <div class="book-now-c">
+                                                            <button @click="bookNow(tourOperatorId, tourPackage.TourPackageId)">Book Now</button>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -84,6 +88,7 @@ export default {
         };
     },
     async created() {
+        this.iframeStatus = this.$store.state.iframeStatus;
         this.year = this.$store.state.year;
         this.tourOperatorId = this.$store.state.tourOperatorId;
         this.packageId = this.$store.state.packageId;
