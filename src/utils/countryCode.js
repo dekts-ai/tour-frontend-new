d320be64749656e913c0b9122ffcc002224d9045
Scott Dean <scott@nativeamericantours.com>
2024-02-27 09:47:35 -0700

    added country code to main

--- src/utils/countryCode.js
+++ src/utils/countryCode.js
@@ -1,6 +1,19 @@
 
 class CountryCodes {
 
+
+  static validatePhoneNumber (number) {
+
+    if( !number || number === "" ){
+      return false;
+    }
+
+     return /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(number.toString());
+     
+  }
+
+
+
   //try our best to format the number like: +1 (111) 222-3333
   static formatPhoneNumber (code, number) {
 
