b459c1ab26b4db05d1f00ee64d113a49b60ea842
scottadean <scott@nativeamericantours.com>
2024-05-16 15:42:37 -0700

    added test local

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -17,3 +17,8 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('can get book online', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.locator('h2.easy-to-find')).toHaveText('Book Online');
+});
--- playwright.config.js
+++ playwright.config.js
@@ -25,10 +25,18 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    video: {
+      mode: 'on',
+      size: { width: 640, height: 480 }
+    }
+
+
+
   },
 
   /* Configure projects for major browsers */
@@ -70,10 +78,12 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    //command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
+    command: process.env.CI ? 'vue-cli-service serve' : 'npm run serve',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+  },
 });
+
 
