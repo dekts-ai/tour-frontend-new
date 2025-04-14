da26b2fcecc8def9c75757a40f60e45474f629c8
Devat Karetha <devat73@gmail.com>
2025-04-14 18:13:39 +0530

    Fixed the warning of the vue prod dev tools

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const fs = require('fs')
 module.exports = {
+  chainWebpack: config => {
+    config.plugin('define').tap(args => {
+      args[0].__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = JSON.stringify(false);
+      args[0].__VUE_PROD_DEVTOOLS__ = JSON.stringify(false);
+      return args;
+    });
+  },
   // // uncommemnt to run in ssl on loncal
   // devServer: {
   //   host: 'localhost',
